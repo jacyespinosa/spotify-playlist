944e7644bbc163419b1a38f1a5b2f7a01e837e32
Jacy Mae Espinosa <jaymespinosa10@gmail.com>
2021-07-22 20:59:36 -0700

    Ask user for a date and return the Top 100 Billboard songs on the date chosen.

---
+++ main.py
@@ -0,0 +1,6 @@
+
+'''ASK USER FOR THEIR DATE PREFERENCE AND RETURN THE TOP 100 BILLBOARD SONGS ON THAT SPECIFIC DATE 
+THE USER CHOSE USING BEAUTFULSOUP '''
+
+user_input = input("Which year do you want to travel to? Please enter the date in this format YYYY-MM-DD:\n")
+
